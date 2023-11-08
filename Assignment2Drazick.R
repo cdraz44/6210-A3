8773625b86d380d8dc57acf43f5ba95b3ff6ffe4
cdraz44 <148305410+cdraz44@users.noreply.github.com>
2023-11-08 10:33:18 -0500

    Add files via upload

---
+++ Assignment2Drazick.R
@@ -0,0 +1,308 @@
+##load packages to be used in this analysis
+
+library(tidyverse)
+library(dbplyr)
+library(ape)
+library(muscle)
+library(DECIPHER)
+library(rentrez)
+
+##searching NCBI Genbank nucleotide database for gene sequences for family Cetacea ensuring we are only finding sequences of the actual genes and not entire genomes
+
+NOTCH3_search <- entrez_search(db = "nuccore", term = "(Cetacea[ORGN] AND NOTCH3[Gene] AND 6000:9000[SLEN])", retmax = 1000)
+
+BRCA1_search <- entrez_search(db = "nuccore", term = "(Cetacea[ORGN] AND BRCA1[Gene] AND 5000:8000[SLEN])", retmax = 1000)
+
+##Fetching data for each gene of interest from NCBI genbank nucleotide database, retrieving data in .fasta format
+
+NOTCH3_fetch <- entrez_fetch(db = "nuccore", id = NOTCH3_search$ids, rettype = "fasta")
+
+BRCA1_fetch <- entrez_fetch(db = "nuccore", id = BRCA1_search$ids, rettype = "fasta")
+
+##Check class to ensure it is a character vector
+
+class(NOTCH3_fetch)
+
+class(BRCA1_fetch)
+
+##Having a look at the data to ensure we have the right sequence data
+
+head(BRCA1_fetch)
+
+head(NOTCH3_fetch)
+
+##write to file, separate and remove Ns from sequence data
+
+write(NOTCH3_fetch, "NOTCH3_fetch.fasta", sep = "\n")
+
+write(BRCA1_fetch, "BRCA1_fetch.fasta", sep = "\n")
+
+##Rewriting to a DNA string set from the .fasta file without any Ns in the sequences
+
+NOTCH3_string <- readDNAStringSet("NOTCH3_fetch.fasta")
+
+BRCA1_string <- readDNAStringSet("BRCA1_fetch.fasta")
+
+##Checking data, ensuring we have proper class and viewing our data
+class(NOTCH3_string)
+head(names(NOTCH3_string))
+
+class(BRCA1_string)
+head(names(BRCA1_string))
+
+##Creating a data frame from our stringset taking values(names) from stringset and plugging them into data frame
+
+dfBRCA1 <- data.frame(BRCA1_Title = names(BRCA1string), 
+                      BRCA1_Sequence = paste(BRCA1string))
+
+dfNOTCH3 <- data.frame(NOTCH3_Title = names(NOTCH3_string),
+                       NOTCH3_Sequence = paste(NOTCH3_string))
+
+##This is some name editing to clean up sequence names and remove unwanted words from species names
+
+dfBRCA1$Species_Name <- word(dfBRCA1$BRCA1_Title, 3L, 4L)
+dfNOTCH3$Species_Name <- word(dfNOTCH3$NOTCH3_Title, 3L, 4L)
+
+##Rearranging data frame columns
+
+dfBRCA1 <- dfBRCA1[, c("BRCA1_Title", "Species_Name", "BRCA1_Sequence")]
+dfNOTCH3 <- dfNOTCH3[, c("NOTCH3_Title", "Species_Name", "NOTCH3_Sequence")]
+
+##Checking dimensions of the dataframe as quality control
+
+dim(dfBRCA1)
+
+dim(dfNOTCH3)
+
+##Checking names of dataframe as quality control
+names(dfBRCA1)
+
+names(dfNOTCH3)
+
+##Getting a summary of sequence lengths to ensure sequence length is appropriate
+
+summary(nchar(dfBRCA1$BRCA1_Sequence))
+
+summary(nchar(dfNOTCH3$NOTCH3_Sequence))
+
+##loading packages for plotting
+
+library(ggplot2)
+
+library(ggtext)
+
+##coding for a histogram displaying the distribution of sequence lengths, to ensure quality control and identify any errors or outliers
+
+BRCA1PLOT<- ggplot(data = dfBRCA1,
+                   mapping = aes(x = nchar(BRCA1_Sequence)))  + 
+  geom_histogram(breaks= seq(5000,8000, by = 75), 
+                 col="blue", aes(fill=..count..)) + scale_fill_distiller(palette= "Spectral") + 
+  labs(title = "Frequency of Sequence Length of BRCA1 in Cetacea", x = "Sequence Length", y = "Number of Species")  + theme_bw()
+
+
+NOTCH3PLOT<- ggplot(data = dfNOTCH3,
+                    mapping = aes(x = whales))  + 
+  geom_histogram(breaks= seq(6000,9000, by = 75), 
+                 col="red", aes(fill=..count..)) + scale_fill_distiller(palette = "Spectral")  +
+  labs(title = "Frequency of Sequence Length of NOTCH3 in Cetacea", x = "Sequence Length", y = "Frequency") + theme_bw() 
+
+##Plotting the pot to visualize
+
+plot(NOTCH3PLOT)
+
+plot(BRCA1PLOT)
+
+##Making sure that sequence IDs are dna string sets in order to run alignment
+dfBRCA1$BRCA1_Sequence2 <- DNAStringSet(dfBRCA1$BRCA1_Sequence)
+
+dfNOTCH3$NOTCH3_Sequence2 <- DNAStringSet(dfNOTCH3$NOTCH3_Sequence)
+
+##checking class -> string set
+class(dfNOTCH3$NOTCH3_Sequence2)
+
+class(dfBRCA1$BRCA1_Sequence2)
+
+##Ensuring species names are inputted so that they can carry forward through the analysis
+names(dfNOTCH3$NOTCH3_Sequence2) <- dfNOTCH3$Species_Name
+
+names(dfBRCA1$BRCA1_Sequence2) <- dfBRCA1$Species_Name
+
+##Double checking that the names have transferred in and checking they are cleaned up
+names(dfNOTCH3$NOTCH3_Sequence2)
+
+names(dfBRCA1$BRCA1_Sequence2)
+
+##Performing sequence alignment
+
+dfBRCA1.alignment <- DNAStringSet(muscle::muscle(dfBRCA1$BRCA1_Sequence2), 
+                                  use.names = TRUE)
+
+dfNOTCH3.alignment <- DNAStringSet(muscle::muscle(dfNOTCH3$NOTCH3_Sequence2),
+                                   use.names = TRUE)
+
+##Checking that alignment worked, with species names 
+
+dfBRCA1.alignment 
+
+dfNOTCH3.alignment
+
+##Opening an alignment in the browser to double check alignment quality
+
+BrowseSeqs(dfBRCA1.alignment)
+
+BrowseSeqs(dfNOTCH3.alignment)
+
+##Changing alignment class to DNA bin to be used in a distance matrix for subsequent cluster analysis
+
+dnaBin.NOTCH3 <- as.DNAbin(dfNOTCH3.alignment)
+
+dnaBin.BRCA1 <- as.DNAbin(dfBRCA1.alignment)
+
+##Checking that it worked and is now DNA bin class
+
+class(dnaBin.NOTCH3)
+
+class(dnaBin.BRCA1)
+
+##Creating distance matrixes
+
+distanceMatrixBRCA <- dist.dna(dnaBin.BRCA1, 
+                               model = chosen.model, 
+                               as.matrix = TRUE, 
+                               pairwise.deletion = TRUE)
+
+distanceMatrixNOTCH3 <- dist.dna(dnaBin.NOTCH3, 
+                                 model = chosen.model, 
+                                 as.matrix = TRUE, 
+                                 pairwise.deletion = TRUE)
+
+##Ensuring that it worked 
+
+head(distanceMatrixBRCA)
+
+head(distanceMatrixNOTCH3)
+
+##Setting values for cluster analysis
+
+missing.data <- 0.01
+
+length.var <- 10
+
+chosen.model <- "TN93"
+
+clustering.threshold <- 0.03
+
+clustering.method <- "UPGMA"
+
+##!!please note using IdClusters here instead of TreeLine!! -Clustering my data using previously chosen cluster values
+clusters.NOTCH3 <- DECIPHER::IdClusters(myDistMatrix = distanceMatrixNOTCH3, 
+                                        method = clustering.method,
+                                        cutoff = clustering.threshold,
+                                        showPlot = TRUE,
+                                        type = "both",
+                                        verbose= TRUE)
+
+clusters.BRCA1 <- DECIPHER::IdClusters(myDistMatrix = distanceMatrixBRCA,
+                                       method = clustering.method,
+                                       cutoff = clustering.threshold,
+                                       type = "both",
+                                       showPlot = TRUE,
+                                       verbose = TRUE)
+
+##Checking class, viewing and checking length of clusters for quality control
+
+class(clusters.BRCA1)
+
+clusters.BRCA1
+
+length(clusters.BRCA1)
+
+
+class(clusters.NOTCH3)
+
+clusters.NOTCH3
+
+length(clusters.NOTCH3)
+
+##Installing package for creating kmeans analysis and cluster plots 
+
+install.packages("cluster")
+library(cluster)
+
+##numbering species names making each a unique name to use as a unique data point on our plot
+rownames(distanceMatrixNOTCH3) = make.names(rownames(distanceMatrixNOTCH3), unique = TRUE)
+
+rownames(distanceMatrixBRCA) = make.names(rownames(distanceMatrixBRCA), unique = TRUE)
+
+##Setting the seed for reproduction
+set.seed(123)
+
+##Finding kmeans values and indicating number of clusters.(I decided on these numbers for k-means clusters through trial and error and looking at kmeans results to decide which values to use for plotting)
+
+kmNOTCH3 <- kmeans(distanceMatrixNOTCH3,3)
+
+kmBRCA1 <- kmeans(distanceMatrixBRCA, 6)
+
+##Viewing kmeans outcomes and evaluating whether this is the right number of clusters
+
+kmNOTCH3
+
+kmBRCA1
+
+##Installing package for kmeans cluster plotting as well as silhouette index plotting
+
+install.packages("factoextra")
+library(factoextra)
+library(viridis)
+
+##Creating kmeans cluster plot in ggplot style, colouring and tweaking default parameters to match the goal of these plots
+
+NOTCH3_cluster <- fviz_cluster(kmNOTCH3, data = distanceMatrixNOTCH3, geom = c("point","text"), 
+                               repel = TRUE, show.clust.cent = TRUE, ellipse.type = "convex", labelsize = 5) + 
+  labs(title= "K-means Analysis of NOTCH3") + theme(panel.background = element_rect(fill = "darkgrey")) + scale_color_viridis_d(option = "H")
+
+BRCA1_cluster <- fviz_cluster(kmBRCA1, data = distanceMatrixBRCA, geom = c("point","text"), 
+                              repel = TRUE, ellipse.type = "convex", labelsize = 8) + 
+  labs(title= "K-means Analysis of BRCA1") + theme(panel.background = element_rect(fill = "darkgrey"))   + scale_color_viridis_d(option = "A") 
+
+##Plotting and viewing of plots
+plot(BRCA1_cluster)
+
+plot(NOTCH3_cluster)
+
+##To ensure we have the right amount of clusters for our k-means analysis, I am creating a plot to tell us the optimal number of clusters for each using silhouette index calculations
+OptNOTCH3 <- fviz_nbclust(distanceMatrixNOTCH3, FUNcluster = kmeans, method = "silhouette", linecolor = "lightpink2")
+
+OptBRCA1 <- fviz_nbclust(distanceMatrixBRCA, FUNcluster = kmeans, method = "silhouette", linecolor = "lightblue2")
+
+##Plotting optimal cluster plot
+plot(OptNOTCH3)
+
+plot(OptBRCA1)
+
+##Changing the number of K-mean clusters
+
+kmNOTCH3 <- kmeans(distanceMatrixNOTCH3,5)
+
+kmBRCA1 <- kmeans(distanceMatrixBRCA, 4)
+
+##Re-making the graphs using this new parameter
+
+NOTCH3_cluster <- fviz_cluster(kmNOTCH3, data = distanceMatrixNOTCH3, geom = c("point","text"), 
+                               repel = TRUE, show.clust.cent = TRUE, ellipse.type = "convex", labelsize = 5) + 
+  labs(title= "K-means Analysis of NOTCH3") + theme(panel.background = element_rect(fill = "darkgrey")) + scale_color_viridis_d(option = "H")
+
+BRCA1_cluster <- fviz_cluster(kmBRCA1, data = distanceMatrixBRCA, geom = c("point","text"), 
+                              repel = TRUE, ellipse.type = "convex", labelsize = 8) + 
+  labs(title= "K-means Analysis of BRCA1") + theme(panel.background = element_rect(fill = "darkgrey"))   + scale_color_viridis_d(option = "A") 
+
+##Plotting and viewing new plot with updated kmeans parameter
+
+plot(NOTCH3_cluster)
+
+plot(BRCA1_cluster)
+
+
+
+
+
